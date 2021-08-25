69c86546f0554d72372d8dc94f72fa82892aeb55
kenwoo9y <ei.1px.9.01@gmail.com>
2021-08-25 23:48:56 +0900

    First commit

---
+++ src/main/java/com/example/springtodosample/SpringTodoSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtodosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtodosample/SpringTodoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtodosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
